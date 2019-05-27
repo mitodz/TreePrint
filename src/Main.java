e1d26436120cc8bb629d574eb11a359fff51d196
mitodz <mitodzzz@gmail.com>
2019-05-27 21:33:12 +0300

    Perhaps

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,10 @@ public class Main {
         for (int i = 0; i < n; i++) {
             a[i] = new Node(sc.nextInt(), sc.nextInt(), sc.nextInt());
         }
+        a[0].printTree(a, a[0]);
         System.out.println();
+        a[0].prePrintTree(a, a[0]);
+        System.out.println();
+        a[0].postPrintTree(a, a[0]);
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -2,49 +2,55 @@ class Node {
     private int number;
     private int left;
     private int right;
-    private static StringBuilder res;
 
     public Node(int number, int left, int right) {
         this.number = number;
         this.left = left;
         this.right = right;
-        res = new StringBuilder();
     }
 
-    public String printTree (Node[] t, Node n) {
-        if (n.getLeft()==-1 && n.getRight()==-1) return res.toString();
-        if (n.getLeft()!=-1) {
-            res.append(n.getNumber()).append(" ");
-            return printTree(t, t[n.getLeft()]);
+    public void printTree(Node[] t, Node n) {
+        if (n.getLeft() != -1) {
+            printTree(t, t[n.getLeft()]);
         }
-        if (n.getRight()!=-1) {
-            res.append(n.getNumber()).append(" ");
-            return printTree(t, t[n.getRight()]);
+        System.out.print(n.getNumber() + " ");
+        if (n.getRight() != -1) {
+            printTree(t, t[n.getRight()]);
         }
-        return res.toString();
     }
+
+    public void prePrintTree(Node[] t, Node n) {
+        System.out.print(n.getNumber() + " ");
+        if (n.getLeft() != -1) {
+            prePrintTree(t, t[n.getLeft()]);
+        }
+        if (n.getRight() != -1) {
+            prePrintTree(t, t[n.getRight()]);
+        }
+    }
+
+    public void postPrintTree(Node[] t, Node n) {
+        if (n.getLeft() != -1) {
+            postPrintTree(t, t[n.getLeft()]);
+        }
+        if (n.getRight() != -1) {
+            postPrintTree(t, t[n.getRight()]);
+        }
+        System.out.print(n.getNumber() + " ");
+
+    }
+
 
     public int getNumber() {
         return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
     }
 
     public int getLeft() {
         return left;
     }
 
-    public void setLeft(int left) {
-        this.left = left;
-    }
-
     public int getRight() {
         return right;
     }
 
-    public void setRight(int right) {
-        this.right = right;
-    }
 }
